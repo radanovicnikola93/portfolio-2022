8e18a7531d8e407eb968245b4d5c83a1953fef56
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-06 18:01:32 +0100

    Added functionality to open or close nav__links on for-phones-and-tablets-only media query

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector(".nav__menu-toggler");
+const navLinks = document.querySelector(".nav__links")
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('open')
+    navLinks.classList.toggle("active-nav-links");
 })
