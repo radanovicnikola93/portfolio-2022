fec1b4842303754d8530e7a3114da9ac6ef21189
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-09 16:17:50 +0100

    Updated delays and starting points of animations

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,23 +1,26 @@
+gsap.registerPlugin(ScrollTrigger); // add scroll trigger animation
+
 // HEADER
 gsap.from("#header-main", { opacity: 0, duration: 1, delay: 0 });
 gsap.from("#header-sub", { opacity: 0, duration: 1, delay: 1 });
 
 // NAV
-gsap.from("#nav-about", { opacity: 0, duration: 0.5, delay: 2});
-gsap.from("#nav-technologies", { opacity: 0, duration: 0.5, delay: 2.5 });
-gsap.from("#nav-projects", { opacity: 0, duration: 0.5, delay: 3 });
-gsap.from("#nav-contact", { opacity: 0, duration: 0.5, delay: 3.5 });
-gsap.from("#nav-logo", { opacity: 0, duration: 0.5, delay: 3.5 });
+gsap.from("#nav-logo", { opacity: 0, duration: 0.5, delay: 2 });
+gsap.from("#nav-about", { opacity: 0, duration: 0.5, delay: 2.5 });
+gsap.from("#nav-technologies", { opacity: 0, duration: 0.5, delay: 3 });
+gsap.from("#nav-projects", { opacity: 0, duration: 0.5, delay: 3.5 });
+gsap.from("#nav-contact", { opacity: 0, duration: 0.5, delay: 4 });
 
-// gsap.from("#nav-toggler", { opacity: 0, duration: 1, delay: 1 });
 
 // PERSONAL SOCIAL MEDIA
 gsap.from("#personal-social-media", {
     opacity: 0,
     duration: 1,
     x: -50,
-    delay: 3.5,
+    delay: 4,
 });
 
 // PERSONAL CONTACTS
-gsap.from("#personal-contact", { opacity: 0, duration: 1, x: 50, delay: 3.5 });
+gsap.from("#personal-contact", { opacity: 0, duration: 1, x: 50, delay: 4 });
+
+
