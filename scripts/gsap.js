fff71174fb2e85fe30ceef7d7f1538b69e5e41d9
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-09 19:45:21 +0100

    Created function timeline and updated previous values with function

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,48 +1,73 @@
 gsap.registerPlugin(ScrollTrigger); // add scroll trigger animation
 
+// TIMELINE FUNCTION
+const timeline = (
+    target,
+    trigger,
+    x,
+    y,
+    opacity,
+    duration,
+    scale,
+    rotate,
+    delay
+) => {
+    let tl = gsap.timeline({
+        // generating a timeline
+        scrollTrigger: {
+            trigger: trigger,
+            toggleActions: "restart none none pause",
+        },
+    });
+
+    tl.from(target, {
+        x: x,
+        y: y,
+        opacity: opacity,
+        duration: duration,
+        scale: scale,
+        rotate: rotate,
+        delay: delay,
+    });
+};
 // HEADER
-gsap.from("#header-main", {duration: 0.5, scale: 1.2})
-gsap.from("#header-sub", { opacity: 0, duration: 0.5, delay: 0.5 });
+timeline("#header-main", "#navigation", 0, 0, 0, 0.7, 150, 0, 0);
+timeline("#header-sub", "#navigation", 0, 0, 0, 0.5, 1, 0, 0.7);
 
 // NAV
-gsap.from("#nav-logo", { opacity: 0, duration: 1.5, delay: 1 , rotate: 180});
-gsap.from("#nav-about", { opacity: 0, duration: 0.3, delay: 1.5 });
-gsap.from("#nav-technologies", { opacity: 0, duration: 0.3, delay: 1.8 });
-gsap.from("#nav-projects", { opacity: 0, duration: 0.3, delay: 2.1 });
-gsap.from("#nav-contact", { opacity: 0, duration: 0.3, delay:  2.4});
-
+timeline("#nav-logo", "#navigation", 0, 0, 0, 1.5, 0, 180, 1);
+timeline("#nav-about", "#navigation", 0, 0, 0, 0.3, 0, 0, 1.5);
+timeline("#nav-technologies", "#navigation", 0, 0, 0, 0.3, 0, 0, 1.8);
+timeline("#nav-projects", "#navigation", 0, 0, 0, 0.3, 0, 0, 2.1);
+timeline("#nav-contact", "#navigation", 0, 0, 0, 0.3, 0, 0, 2.4);
 
 // PERSONAL SOCIAL MEDIA
-gsap.from("#personal-social-media", {
-    opacity: 0,
-    duration: 1,
-    x: -50,
-    delay: 2.4,
-});
+timeline(
+    "#personal-social-media",
+    "#personal-social-media",
+    -50,
+    0,
+    0,
+    1,
+    0,
+    0,
+    2.4
+);
 
 // PERSONAL CONTACTS
-gsap.from("#personal-contact", { opacity: 0, duration: 1, x: 50, delay: 2.4 });
+timeline(
+    "#personal-contact",
+    "#personal-contact",
+    50,
+    0,
+    0,
+    1,
+    0,
+    0,
+    2.4
+);
 
 // ABOUT ME
-
-gsap.from("#header-1", {
-    scrollTrigger: {
-        trigger: "main"
-    },
-    duration: 1.1,
-    x: -500,
-});
-gsap.from("#about-me-presentation", {
-    scrollTrigger: {
-        trigger: "main",
-    },
-    duration: 1,
-    y: 100,
-});
-gsap.from("#about-me-image", {
-    scrollTrigger: {
-        trigger: "main",
-    },
-    duration: 1,
-    y: -100,
-});
+timeline("#header-1", "main", -500, 0, 1, 0.7, 0, 0, 0);
+timeline("#about-me-presentation", "#header-1", 0, 0, 0, 0.7, 1, 0, 0.9);
+timeline("#about-me-image", "#header-1", 0, 0, 0, 0.7, 1, 0, 1.6);
