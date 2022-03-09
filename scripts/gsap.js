9faa288613b1bae4401b027fc82b86e370083942
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-09 09:51:04 +0100

    Animated nav

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,3 +1,7 @@
-const navigation = '#navigation' // nav
-
-gsap.from(navigation, {opacity: 0, duration: 2, y: -50})
+// NAV
+gsap.from('#navigation', {opacity: 0, duration: 0.5, y: -50});
+gsap.from('#nav-logo', { opacity: 0, duration: 2, delay: 0.5});
+gsap.from('#nav-about', { opacity:0, duration: 0.5, delay: 0.5})
+gsap.from('#nav-technologies', { opacity:0, duration: 0.5, delay: 1})
+gsap.from('#nav-projects', { opacity:0, duration: 0.5, delay: 1.5})
+gsap.from("#nav-contact", { opacity: 0, duration: 0.5, delay: 2 });
