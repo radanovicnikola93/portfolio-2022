c457998ab98d85235272717437d0da8041ba0e78
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-09 18:57:39 +0100

    Updated delays and added scroll trigger

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,14 +1,15 @@
 gsap.registerPlugin(ScrollTrigger); // add scroll trigger animation
 
 // HEADER
-gsap.from("#header-sub", { opacity: 0, duration: 1, delay: 1 });
+gsap.from("#header-main", {duration: 0.5, scale: 1.2})
+gsap.from("#header-sub", { opacity: 0, duration: 0.5, delay: 0.5 });
 
 // NAV
-gsap.from("#nav-logo", { opacity: 0, duration: 0.5, delay: 2 });
-gsap.from("#nav-about", { opacity: 0, duration: 0.5, delay: 2.5 });
-gsap.from("#nav-technologies", { opacity: 0, duration: 0.5, delay: 3 });
-gsap.from("#nav-projects", { opacity: 0, duration: 0.5, delay: 3.5 });
-gsap.from("#nav-contact", { opacity: 0, duration: 0.5, delay: 4 });
+gsap.from("#nav-logo", { opacity: 0, duration: 1.5, delay: 1 , rotate: 180});
+gsap.from("#nav-about", { opacity: 0, duration: 0.3, delay: 1.5 });
+gsap.from("#nav-technologies", { opacity: 0, duration: 0.3, delay: 1.8 });
+gsap.from("#nav-projects", { opacity: 0, duration: 0.3, delay: 2.1 });
+gsap.from("#nav-contact", { opacity: 0, duration: 0.3, delay:  2.4});
 
 
 // PERSONAL SOCIAL MEDIA
@@ -16,26 +17,32 @@ gsap.from("#personal-social-media", {
     opacity: 0,
     duration: 1,
     x: -50,
-    delay: 4.5,
+    delay: 2.4,
 });
 
 // PERSONAL CONTACTS
-gsap.from("#personal-contact", { opacity: 0, duration: 1, x: 50, delay: 4.5 });
+gsap.from("#personal-contact", { opacity: 0, duration: 1, x: 50, delay: 2.4 });
 
 // ABOUT ME
 
 gsap.from("#header-1", {
-    opacity: 0,
+    scrollTrigger: {
+        trigger: "main"
+    },
     duration: 1.1,
     x: -500,
 });
 gsap.from("#about-me-presentation", {
-    opacity: 0,
+    scrollTrigger: {
+        trigger: "main",
+    },
     duration: 1,
     y: 100,
 });
 gsap.from("#about-me-image", {
-    opacity: 0,
+    scrollTrigger: {
+        trigger: "main",
+    },
     duration: 1,
     y: -100,
 });
