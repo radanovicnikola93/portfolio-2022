cbe2a816f2490f723301dde4d089ec4f498bebc1
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-10 09:20:52 +0100

    Added other animations only for mobile

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,38 +1,39 @@
+gsap.registerPlugin(ScrollTrigger); // add scroll trigger animation
+
+// TIMELINE FUNCTION
+const timeline = (
+    target,
+    trigger,
+    x,
+    y,
+    opacity,
+    duration,
+    scale,
+    rotate,
+    delay
+) => {
+    let tl = gsap.timeline({
+        // generating a timeline
+        scrollTrigger: {
+            trigger: trigger,
+            toggleActions: "restart none none pause",
+        },
+    });
+
+    tl.from(target, {
+        x: x,
+        y: y,
+        opacity: opacity,
+        duration: duration,
+        scale: scale,
+        rotate: rotate,
+        delay: delay,
+    });
+};
+
 function onlyDesktop(px) {
+    // ---------------------------------------------------- DESKTOP ---------------------------------------------
     if (px.matches) {
-        gsap.registerPlugin(ScrollTrigger); // add scroll trigger animation
-
-        // TIMELINE FUNCTION
-        const timeline = (
-            target,
-            trigger,
-            x,
-            y,
-            opacity,
-            duration,
-            scale,
-            rotate,
-            delay
-        ) => {
-            let tl = gsap.timeline({
-                // generating a timeline
-                scrollTrigger: {
-                    trigger: trigger,
-                    toggleActions: "restart none none pause",
-                },
-            });
-
-            tl.from(target, {
-                x: x,
-                y: y,
-                opacity: opacity,
-                duration: duration,
-                scale: scale,
-                rotate: rotate,
-                delay: delay,
-            });
-        };
-
         // HEADER
         timeline("#header-main", "#navigation", 0, 0, 0, 0.7, 150, 0, 0);
         timeline("#header-sub", "#navigation", 0, 0, 0, 0.5, 1, 0, 0.7);
@@ -160,7 +161,101 @@ function onlyDesktop(px) {
             0,
             0.3
         );
+        // ----------------------------------------------------------------------------------
+    } else {
+        // ----------------------------------------------------MOBILE---------------------------------------------
+        // NAV
+        timeline("#nav-logo", "#navigation", 0, 0, 0, 1.5, 0, 180, 0);
 
+        // PERSONAL SOCIAL MEDIA
+        timeline(
+            "#personal-social-media",
+            "#personal-social-media",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
+
+        // PERSONAL CONTACTS
+        timeline(
+            "#personal-contact",
+            "#personal-contact",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
+
+        // ABOUT ME
+        timeline("#header-1", "main", 0, 0, 0, 1.2, 1, 0, 0.5);
+        timeline(
+            "#about-me-presentation",
+            "#header-1",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
+        timeline("#about-me-image", "#header-1", 0, 0, 0, 1.2, 1, 0, 0.5);
+
+        // TECHNOLOGIES
+        timeline("#header-2", "#header-2", 0, 0, 0, 1.2, 1, 0, 0.5);
+        for (let i = 1; i < 9; i++) {
+            // for every column add navigation
+            timeline(`#col-${i}`, `#col-${i}`, 0, 0, 0, 1.2, 1, 0, 0.5);
+        }
+
+        // PROJECTS
+        timeline("#header-3", "#header-3", 0, 0, 0, 1.2, 1, 0, 0.5);
+        timeline(
+            "#project-header-1",
+            "#project-header-1",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
+        timeline(
+            "#project-header-2",
+            "#project-header-2",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
+        for (let i = 1; i < 11; i++) {
+            timeline(`#project-${i}`, `#project-${i}`, 0, 0, 0, 1.2, 1, 0, 0.5);
+        }
+
+        // CONTACT
+        timeline("#header-4", "#header-4", -500, 0, 1, 0.7, 0, 0, 0);
+        timeline(
+            "#contact-container",
+            "#contact-container",
+            0,
+            0,
+            0,
+            1.2,
+            1,
+            0,
+            0.5
+        );
     }
 }
 
